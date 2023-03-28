03608c0eb799ad1a9a74b0eff1a0f10acede7080
Hannah Williams <hwilli72@vols.utk.edu>
2023-03-28 10:43:06 -0400

    Create first function

--- maplab/__init__.py
+++ maplab/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Hannah Williams"""
 __email__ = 'hwilli72@vols.utk.edu'
 __version__ = '0.2.0'
+
+from .maplab import *
--- maplab/maplab.py
+++ maplab/maplab.py
@@ -1,1 +1,12 @@
 """Main module."""
+
+import string
+import random
+
+def random_string(length, upper=False, digits=False):
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    return ''.join(random.choice(letters) for i in range(length))
