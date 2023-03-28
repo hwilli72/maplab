30a99b98e1375d3c31df1b278276207dd701a04e
Hannah Williams <hwilli72@vols.utk.edu>
2023-03-28 10:55:05 -0400

    Added documentation to my function.

--- maplab/maplab.py
+++ maplab/maplab.py
@@ -4,6 +4,16 @@ import string
 import random
 
 def random_string(length, upper=False, digits=False):
+    """Generates a random string of a given length.
+
+    Args:
+        length (_type_): The length of the string to generate.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+
+    Returns:
+        str: random string
+    """    
     letters = string.ascii_lowercase
     if upper:
         letters += string.ascii_uppercase
